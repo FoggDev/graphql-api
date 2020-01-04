56d610d5dba52bfa810cdbf50f3cab790d399ce9
Carlos Santana <carlos.santana@dev.education>
2020-01-04 15:00:17 -0800

    V16 - Adding orderBy, direction, limit & offset

--- src/graphql/resolvers/post.js
+++ src/graphql/resolvers/post.js
@@ -1,16 +1,39 @@
 export default {
   Query: {
-    getPosts: (parent, args, { models }) => {
-      return models.Post.findAll({
+    getPostsCount: (_, args, { models }) => {
+      return models.Post.count().then(count => ({
+        count
+      }))
+    },
+    getPosts: (_, { options = {} }, { models }) => {
+      const {
+        orderBy = 'createdAt',
+        direction = 'DESC',
+        limit = false,
+        offset = false
+      } = options
+
+      const args = {
+        order: [[orderBy, direction]],
         include: [{
           model: models.Tag,
           as: 'tags'
         }]
-      })
+      }
+
+      if (limit > 0) {
+        args.limit = limit
+      }
+
+      if (offset > 0) {
+        args.offset = offset
+      }
+
+      return models.Post.findAll(args)
     }
   },
   Mutation: {
-    createPost: (parent, { input }, { models }) => {
+    createPost: (_, { input }, { models }) => {
       return models.Post.create({ ...input }, {
         include: [{
           model: models.Tag,
