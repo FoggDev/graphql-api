ed99c7d7d78cece2ce94f67899529315f701b0c7
Carlos Santana <carlos.santana@dev.education>
2020-01-27 19:07:07 -0800

    Adding options to getPosts and adding getPostById

--- src/graphql/resolvers/post.js
+++ src/graphql/resolvers/post.js
@@ -1,16 +1,33 @@
 export default {
   Query: {
-    getPosts: (parent, args, { models }) => {
-      return models.Post.findAll({
+    getPosts: (_, { options: { orderBy, direction = 'ASC', limit, offset } }, { models }) => {
+      const args = {
         include: [{
           model: models.Tag,
           as: 'tags'
         }]
-      })
+      }
+
+      if (orderBy && direction) {
+        args.order = [[orderBy, direction]]
+      }
+
+      if (limit) {
+        args.limit = Number(limit)
+      }
+
+      if (offset) {
+        args.offset = Number(offset)
+      }
+
+      return models.Post.findAll(args)
+    },
+    getPostById: (_, { id }, { models }) => {
+      return models.Post.findByPk(id)
     }
   },
   Mutation: {
-    createPost: (parent, { input }, { models }) => {
+    createPost: (_, { input }, { models }) => {
       return models.Post.create({ ...input }, {
         include: [{
           model: models.Tag,
